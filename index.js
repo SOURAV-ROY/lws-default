79646841dcde4a4d9c0195187e6cb2e23840503f
SOURAV ROY <souravroy.info13@gmail.com>
2021-04-26 14:59:48 +0600

    json,raw,text,urlencoded,static METHODS is Done

--- index.js
+++ index.js
@@ -1,13 +1,25 @@
 const express = require('express',);
-require('dotenv');
+// const dotenv = require('dotenv');
 
 const app = express();
 
+app.use(express.json());
+// app.use(express.raw());
+// app.use(express.text());
+// app.use(express.urlencoded());
+
+app.use(express.static(`${__dirname}/public/`, {
+    index: 'home.html',
+}));
+
 app.get('/', (req, res) => {
+    // console.log( typeof req.body);
     res.send('Hello Sourav');
 });
 
 app.post('/', (req, res) => {
+    // console.log(req.body.toString());
+    console.log(req.body);
     res.send('This is post url with method');
 });
 
