54773f3fa414ebc415660e866a07c549dee69556
SOURAV ROY <souravroy.info13@gmail.com>
2021-04-27 17:14:06 +0600

    app.route() Is DONE

--- index.js
+++ index.js
@@ -2,52 +2,88 @@ const express = require('express',);
 // const dotenv = require('dotenv');
 
 const app = express();
-const admin = express();
+// const admin = express();
 
 // admin.on('mount', function (parent) {
 //     console.log('Admin Mounted')
 //     console.log(parent) // refers to the parent app
 // })
+app.route('/api/v1/first')
+    .get((req, res) => {
+        res.send('Hello Home');
+    })
+    .post((req, res) => {
+        res.send('Hello Post');
+    })
+    .put((req, res) => {
+        res.send('Hello Put');
+    })
+    .delete((req, res) => {
+        res.send('Hello Delete');
+    })
 
-const router = express.Router({
+// app.get('/', (req, res) => {
+//     res.send('Hello Home');
+// });
+// app.post('/', (req, res) => {
+//     res.send('Hello Post Page');
+// });
+// app.put('/', (req, res) => {
+//     res.send('Hello PUT Page');
+// });
+// app.delete('/', (req, res) => {
+//     res.send('Hello Delete Page');
+// });
+
+/** const blog = express()
+ const blogAdmin = express()
+
+ app.use('/blogs', blog)
+ blog.use('/admins', blogAdmin)
+
+ console.dir(app.path()) // ''
+ console.dir(blog.path()) // '/blogs'
+ console.dir(blogAdmin.path()) // '/blogs/admins'
+
+ const router = express.Router({
     caseSensitive: true
 });
-app.use(router);
+ app.use(router);
 
-app.use(express.json());
-// app.use(express.raw());
-// app.use(express.text());
-// app.use(express.urlencoded());
+ app.use(express.json());
+ // app.use(express.raw());
+ // app.use(express.text());
+ // app.use(express.urlencoded());
 
-app.use(express.static(`${__dirname}/public/`, {
+ app.use(express.static(`${__dirname}/public/`, {
     index: 'home.html',
 }));
 
-app.locals.title = 'My Express App';
+ app.locals.title = 'My Express App';
 
-app.use('/admin', admin);
+ app.use('/admin', admin);
 
-app.enable('case sensitive routing');
-admin.enable('case sensitive routing');
-app.disable('case sensitive routing');
-admin.disable('case sensitive routing');
+ app.enable('case sensitive routing');
+ admin.enable('case sensitive routing');
+ app.disable('case sensitive routing');
+ admin.disable('case sensitive routing');
 
-admin.get('/dashboard/hello', (req, res) => {
+ admin.get('/dashboard/hello', (req, res) => {
     console.log(admin.mountpath);
     res.send('Hello Admin Dashboard');
 });
 
-app.all('/all', (req, res) => {
+ app.all('/all', (req, res) => {
     res.send('Accept All Methods Here');
 });
 
-app.get('/', (req, res) => {
+ app.get('/', (req, res) => {
     // console.log( typeof req.body);
     console.log(app.locals.title);
     res.send('Hello Sourav');
 });
 
-app.param('id', (req, res, next, id) => {
+ app.param('id', (req, res, next, id) => {
     const user = {
         userId: id,
         userName: 'Bangladesh',
@@ -56,22 +92,22 @@ app.param('id', (req, res, next, id) => {
     next();
 });
 
-app.get('/user/:id', (req, res) => {
+ app.get('/user/:id', (req, res) => {
     console.log(req.userDetails);
     res.send('Single User Route');
 });
 
-router.get('/about', (req, res) => {
+ router.get('/about', (req, res) => {
     // console.log( typeof req.body);
     res.send('About Page');
 });
 
-app.post('/', (req, res) => {
+ app.post('/', (req, res) => {
     // console.log(req.body.toString());
     console.log(req.body);
     res.send('This is post url with method');
 });
-
+ */
 
 let PORT = process.env.PORT || 2021;
 app.listen(PORT, () => {
