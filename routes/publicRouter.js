bfa661526f635a8997219b79e4703f6e7fdf8102
SOURAV ROY <souravroy.info13@gmail.com>
2021-05-11 22:30:32 +0600

    publicRouter.param() Behaviour Update

--- routes/publicRouter.js
+++ routes/publicRouter.js
@@ -9,19 +9,30 @@ const log = (req, res, next) => {
 
 // publicRouter.all('*', log);
 
-publicRouter.param('user', (req, res, next, id) => {
-    req.user = id === '1' ? 'SOURAV' : 'Anonymous';
-    console.log('I am call once time');
-    next();
-});
+// publicRouter.param('user', (req, res, next, id) => {
+//     req.user = id === '1' ? 'SOURAV' : 'Anonymous';
+//     console.log('I am call once time');
+//     next();
+// });
 
-publicRouter.get('/:user', (req, res, next) => {
-    console.log('Another call same route');
-    next();
+publicRouter.param((param, option) => (req, res, next, value) => {
+    if (value === option) {
+        next();
+    } else {
+        res.sendStatus(403);
+    }
 })
 
+publicRouter.param('user', '10');
+
+// publicRouter.get('/:user', (req, res, next) => {
+//     console.log('Another call same route');
+//     next();
+// })
+
 publicRouter.get('/:user', (req, res) => {
-    res.send(`Hello ${req.user}`);
+    // res.send(`Hello ${req.user}`);
+    res.send(`Hello SOURAV`);
 })
 
 publicRouter.get('/', (req, res) => {
