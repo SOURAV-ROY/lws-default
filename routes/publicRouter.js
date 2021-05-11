41c6c0fb2e04b1a21a61512df32b2a2e43b7c12e
SOURAV ROY <souravroy.info13@gmail.com>
2021-05-11 21:31:23 +0600

    Log Set In Specific Router with before/after router

--- routes/publicRouter.js
+++ routes/publicRouter.js
@@ -2,9 +2,18 @@ const express = require('express');
 
 const publicRouter = express.Router();
 
+const log = (req, res, next) => {
+    console.log("All Log Catch From Here");
+    next();
+}
+
+// publicRouter.all('*', log);
+
 publicRouter.get('/', (req, res) => {
     res.send("Public Dashboard From Public Router");
 })
+
+publicRouter.all('*', log);
 
 publicRouter.get('/login', (req, res) => {
     res.send("Public Login From Public Router");
