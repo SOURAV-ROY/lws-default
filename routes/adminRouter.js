70a22bda217d1a23e2c5d00b369367c126dcbb81
SOURAV ROY <souravroy.info13@gmail.com>
2021-05-18 11:54:44 +0600

    req log console

--- routes/adminRouter.js
+++ routes/adminRouter.js
@@ -3,10 +3,12 @@ const express = require('express');
 const adminRouter = express.Router();
 
 adminRouter.get('/', (req, res) => {
+    console.log(`${new Date(Date.now()).toLocaleString().cyan} - ${req.method.bgBlue.bold} - ${req.originalUrl.bgMagenta} - ${req.protocol.green} - ${req.ip.rainbow}`);
     res.send("Admin Dashboard From Admin Router");
 })
 
 adminRouter.get('/login', (req, res) => {
+    console.log(`${new Date(Date.now()).toLocaleString().cyan} - ${req.method.bgBlue.bold} - ${req.originalUrl.bgMagenta} - ${req.protocol.green} - ${req.ip.rainbow}`);
     res.send("Admin Login From Admin Router");
 })
 
