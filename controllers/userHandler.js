c120f5589017d8d4b7294769c272c87a05e3f165
SOURAV ROY <souravroy.info13@gmail.com>
2021-05-20 14:03:51 +0600

    User Created & Password bcrypt

--- controllers/userHandler.js
+++ controllers/userHandler.js
@@ -1,11 +1,32 @@
 const express = require('express');
+const bcrypt = require('bcrypt');
 const User = require('../models/UserModel');
 
 const todoRouter = express.Router();
 
-// Get @Todo All
-todoRouter.get('/', (req, res) => {
-    res.send('User handler');
+// POST User Signup
+todoRouter.post('/signup', async (req, res) => {
+
+    try {
+
+        const hashedPassword = await bcrypt.hash(req.body.password, 10);
+
+        const newUser = new User({
+            name: req.body.name,
+            username: req.body.username,
+            password: hashedPassword
+        });
+        // save() is a instance method ******************88
+        await newUser.save();
+        res.status(200).json(
+            {message: 'Signup Was Successfully'}
+        );
+    } catch {
+        res.status(500).json(
+            {error: 'Signup Failed'}
+        );
+    }
+
 });
 
 module.exports = todoRouter;
